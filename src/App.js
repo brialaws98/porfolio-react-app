83db54d15215c0c8415ba4e0a6533a805fe8ec58
brialaws98 <brianalaws98@gmail.com>
2021-10-02 19:22:29 -0400

    Edit the index and App.js component
    Got rid of all the unnecessary code and made it simple.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
-import navbar from './navBar';
+import navbar from "./navBar";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
+
     <navBar />
-      
-      </header>
+
     </div>
   );
 }
 
-export default App;
