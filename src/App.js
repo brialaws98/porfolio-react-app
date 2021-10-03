b222cb8effd203b12c86866eb333405e36c8a934
brialaws98 <brianalaws98@gmail.com>
2021-10-03 09:32:38 -0400

    Add GitHub link
    I added the link to my GitHub account so that it can be easily accessed for others to see the work.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import navbar from "./navBar";
 
 export default function App() {
   return (
-    <div className="App">
+   <div className="App">
 
     <navBar />
 
-    </div>
-  );
+
+    <footer>
+   <p>
+<a href="https://github.com/brialaws98/portfolio-react-app">
+ Open Source</a> by <em>Briana Laws</em>
+</p>
+</footer>
+</div>
+  ); 
 }
 
